253ded07eacc90c72f0c6e615fc943a851ac6929
geomin12 <geomin12@amd.com>
2025-08-20 14:11:21 -0700

    Adding setup script

---
+++ README.md
@@ -0,0 +1,13 @@
+# CI Runner Setup
+
+## Windows
+
+(in admin powershell)
+
+- Run `windows.ps1`
+- Make sure `bash` is added to Path, like `C:\Program Files\Git\bin`
+- Run GitHub set up steps
+  - `./config.cmd --url https://github.com/ROCm --token {TOKEN} --windowslogonaccount --windowslogonpassword --no-default-labels --labels {LABEL}`
+  - Make sure to allow security access to group in config step
+- Run this: https://docs.github.com/en/actions/how-tos/manage-runners/self-hosted-runners/configure-the-application?platform=windows to start service
+- In CMD, run `powershell "(Get-Service actions.runner.*).name"`, then `sc config "NAME_OF_YOUR_SERVICE" obj= "NT AUTHORITY\SYSTEM" type= own`
---
+++ windows.ps1
@@ -0,0 +1,32 @@
+# Please run this script as admin
+
+Set-ExecutionPolicy RemoteSigned
+
+# Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install visualstudio2022buildtools -y --params "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.Windows11SDK.22621"
+choco install git.install -y --params "'/GitAndUnixToolsOnPath'"
+choco install cmake --version=3.31.0 -y
+choco install ninja -y
+choco install ccache -y
+choco install python -y
+choco install strawberryperl -y
+
+# https://github.com/shivammathur/setup-php/wiki/Requirements-for-self-hosted-runners
+net user $env:USERNAME | findstr "Local Group Memberships"
+# Setup Latest Powershell Core using windows Powershell.
+Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -AddToPath"
+# Install 7-Zip
+choco install -y --force 7zip.install
+
+# runner setup
+mkdir C:\runner; cd C:\runner
+# Download the latest runner package
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-win-x64-2.328.0.zip -OutFile actions-runner-win-x64-2.328.0.zip
+if((Get-FileHash -Path actions-runner-win-x64-2.328.0.zip -Algorithm SHA256).Hash.ToUpper() -ne 'a73ae192b8b2b782e1d90c08923030930b0b96ed394fe56413a073cc6f694877'.ToUpper()){ throw 'Computed checksum did not match' }
+# Extract the installer
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.328.0.zip", "$PWD")
+
+# install pyyaml globally
+pip install pyyaml
