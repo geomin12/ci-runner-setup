0d7afb33b98802daec29923ea73ade129be79224
Geo Min <geomin12@amd.com>
2025-09-19 07:07:29 -0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,3 +1,4 @@
+sudo apt install curl
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.328.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.328.0.tar.gz
