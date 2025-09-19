ff283454cd32271c82e0d0b68f704c42006cbff4
Geo Min <geomin12@amd.com>
2025-09-19 06:49:49 -0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -2,7 +2,21 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.328.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.328.0.tar.gz
 ./config.sh --url https://github.com/ROCm --token {TOKEN} --no-default-labels --labels {LABEL}
-usermod -a -G sudo "$(id -un)"
+sudo usermod -a -G sudo "$(id -un)"
 echo "%sudo ALL = (ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 sudo ./svc.sh install root
-sudo ./svc.sh start
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl status docker
